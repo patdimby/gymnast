3c6a071271df6a3259cc13ccae284ed8608e6495
RANOELISON Dimbisoa Patrick <patdimby@outlook.fr>
2025-04-26 04:17:52 +0300

    init
    project initialize.

---
+++ src/main/java/com/news/gymnast/GymnastApplication.java
@@ -0,0 +1,13 @@
+package com.news.gymnast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymnastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymnastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/news/gymnast/GymnastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.news.gymnast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymnastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
